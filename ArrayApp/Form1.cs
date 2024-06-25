3d8ecf5383cf546d9488b3b3868628b2516678cf
fontrott <144059131+fontrott@users.noreply.github.com>
2024-06-26 00:21:04 +0300

    Add files via upload

---
+++ ArrayApp/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArrayApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            try 
+            {
+                string input = txtArrayInput.Text;
+                if (String.IsNullOrEmpty(input))
+                {
+                    MessageBox.Show("Строка пустая", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtArrayInput.Clear();
+                    txtRange.Clear();
+                    txtSortedArray.Clear();
+                }
+                else
+                {
+                    int[] array = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+                    ShellSortAndRangeArray<int> arrayHandler = new ShellSortAndRangeArray<int>(array);
+                    arrayHandler.ShellSort();
+                    txtSortedArray.Text = String.Join(", ", arrayHandler.GetArray());
+                }
+            }
+            catch (FormatException fe) 
+            {
+                MessageBox.Show("Вы ввели что-то непонятное " + fe, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtArrayInput.Clear();
+                txtRange.Clear();
+                txtSortedArray.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Что-то не так! " + ex, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtArrayInput.Clear();
+                txtRange.Clear();
+                txtSortedArray.Clear();
+            }
+        }
+        private void btn_range_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string input = txtArrayInput.Text;
+                if (String.IsNullOrEmpty(input))
+                {
+                    MessageBox.Show("Вы ничего не ввели в textbox", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtArrayInput.Clear();
+                    txtRange.Clear();
+                    txtSortedArray.Clear();
+                }
+                double[] array = input.Split(' ').Select(s => Double.Parse(s.Trim())).ToArray();
+
+                ShellSortAndRangeArray<double> arrayHandler = new ShellSortAndRangeArray<double>(array);
+                dynamic range = arrayHandler.GetRange();
+
+                txtRange.Text = range.ToString();
+            }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Вы ввели что-то непонятное " + fe, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtArrayInput.Clear();
+                txtRange.Clear();
+                txtSortedArray.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Что-то не так! " + ex, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                txtArrayInput.Clear();
+                txtRange.Clear();
+                txtSortedArray.Clear();
+            }
+        }
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtArrayInput.Clear();
+            txtRange.Clear();
+            txtSortedArray.Clear();
+        }
+        private void оРазработчикеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ArrayApp/ShellSortAndRangeArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayApp
+{
+    public class ShellSortAndRangeArray<T> where T : IComparable
+    {
+        private T[] array;
+        public ShellSortAndRangeArray(T[] array)
+        {
+            this.array = array;
+        }
+        public void ShellSort()
+        {
+            int n = array.Length;
+            int gap = n / 2;
+
+            while (gap > 0)
+            {
+                for (int i = gap; i < n; i++)
+                {
+                    T temp = array[i];
+                    int j;
+                    for (j = i; j >= gap && array[j - gap].CompareTo(temp) > 0; j -= gap)
+                    {
+                        array[j] = array[j - gap];
+                    }
+                    array[j] = temp;
+                }
+                gap /= 2;
+            }
+        }
+        public dynamic GetRange()
+        {
+            dynamic min = array[0];
+            dynamic max = array[0];
+            foreach (T item in array)
+            {
+                if (item.CompareTo(min) < 0)
+                {
+                    min = item;
+                }
+                if (item.CompareTo(max) > 0)
+                {
+                    max = item;
+                }
+            }
+            return max - min;
+        }
+        public T[] GetArray()
+        {
+            return array;
+        }
+    }
+}
